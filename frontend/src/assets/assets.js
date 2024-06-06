542d26eaa14cd23be3605723953cccf8e7458f90
vidhimerai <vidhimerai16@gmail.com>
2024-06-06 15:06:54 -0400

    Added logo image to the assets

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,7 +1,9 @@
 // Here I import all the images that I want to use
 import login from './login.jpg'
+import Logo from './Logo.png'
 
 // Here I export all the images as an object so I can use this object where I have to use these images
 export const assets = {
     login, // Reference: https://www.google.com/url?sa=i&url=https%3A%2F%2Fin.pinterest.com%2Fpin%2Fhot-dogs--633952085042219368%2F&psig=AOvVaw1fMJfPrN5ggET9LDk2BQUx&ust=1716947443537000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLjqkP6dr4YDFQAAAAAdAAAAABAE
+    Logo,  // I made this logo by myself
 }
