62beea807129cc373d576d6492abf63dbfbf48d5
chelshiyapatel <chelshiyapatel26@gmail.com>
2024-06-06 15:27:26 -0400

    Added the code for Footer.jsx file

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; 2024 FreshBite Subs. All rights reserved.</p>
+        <p>Contact: info@freshbitesubs.com</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
