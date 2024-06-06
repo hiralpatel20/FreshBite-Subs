b96f341786c094153f5e73c3e5111de520744842
chelshiyapatel <chelshiyapatel26@gmail.com>
2024-06-06 15:31:50 -0400

    Added the images into the assets folder

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -5,6 +5,22 @@ import banner1 from './banner1.jpg'
 import banner2 from './banner2.jpg'
 import banner3 from './banner3.jpg'
 import banner4 from './banner4.jpg'
+import veggie from './veggie.jpg'
+import AssortedSub from './AssortedSub.jpg'
+import Ham from './Ham.jpeg'
+import AlbacoreTuna from './AlbacoreTuna.jpeg'
+import BuffaloChicken from './BuffaloChicken.jpg'
+import CanadianClub from './CanadianClub.jpg'
+import ChickenParm from './ChickenParm.jpg'
+import Chips from './Chips.jpeg'
+import ChoclateCookie from './ChoclateCookie.jpeg'
+import garden from './garden.jpg'
+import lousina from './lousina.jpeg'
+import R from './R.jpg'
+import Seafood from './Seafood.jpg'
+import steak from './steak.jpg'
+import texmex from './texmex.jpg'
+import UltimateClub from './UltimateClub.jpg'
 
 // Here I export all the images as an object so I can use this object where I have to use these images
 export const assets = {
@@ -14,4 +30,20 @@ export const assets = {
     banner2, // Reference: https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Fphoto%2F23183589-sandwich-with-ham-cheese-tomato-and-lettuce-on-black-background-ai-generative-image&psig=AOvVaw0j3cQhQZsfmTR6E64ORhgW&ust=1716755833306000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOibopDUqYYDFQAAAAAdAAAAABAZ
     banner3, // Reference: https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Fphoto%2F23185368-sandwich-with-ham-cheese-tomato-and-lettuce-on-black-background-ai-generative-image&psig=AOvVaw0j3cQhQZsfmTR6E64ORhgW&ust=1716755833306000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOibopDUqYYDFQAAAAAdAAAAABAp
     banner4, // Reference: https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Fphoto%2F23185368-sandwich-with-ham-cheese-tomato-and-lettuce-on-black-background-ai-generative-image&psig=AOvVaw0j3cQhQZsfmTR6E64ORhgW&ust=1716755833306000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOibopDUqYYDFQAAAAAdAAAAABAp
+    veggie,  // Reference: https://storage.googleapis.com/ueat-assets/d40e1e54-91b5-4b6b-866a-3d27a54246e4.jpg
+    AssortedSub, // Reference: https://storage.googleapis.com/ueat-assets/91c7dc24-5156-4b1b-b330-18f1fd890818.jpg
+    Ham, // Reference: https://storage.googleapis.com/ueat-assets/d40e1e54-91b5-4b6b-866a-3d27a54246e4.jpg
+    AlbacoreTuna, // Reference: https://storage.googleapis.com/ueat-assets/279efd79-3bf2-4065-bc2b-7c8bcf91b55e.jpg
+    BuffaloChicken, // Reference: https://storage.googleapis.com/ueat-assets/a0152cac-771a-4096-b726-7844117f4a0e.jpg
+    CanadianClub, // Reference: https://storage.googleapis.com/ueat-assets/066b6070-b8fb-492e-b000-87f493611237.jpg
+    ChickenParm, // Reference: https://storage.googleapis.com/ueat-assets/ff2aeb4b-58e2-4bff-9e6b-3644217b666f.jpg
+    Chips, // Reference: https://storage.googleapis.com/ueat-assets/1b388aad-1e1d-40f8-8a2a-8689aad9aec2.jpg
+    ChoclateCookie, // Reference: https://storage.googleapis.com/ueat-assets/c55051b3-802e-47f2-837a-1d544ba15c0f.jpg
+    garden, // Reference: https://www.google.com/imgres?q=gardein%E2%84%A2%20Meatless%20Meatballs%20mr%20sub&imgurl=https%3A%2F%2Fb2038387.smushcdn.com%2F2038387%2Fwp-content%2Fuploads%2F2019%2F09%2Ftexmex.jpg%3Flossy%3D1%26strip%3D1%26webp%3D1&imgrefurl=https%3A%2F%2Fmrsub.ca%2Fmeatless%2F&docid=tsbSiC8Kt3L9NM&tbnid=Gy26Pf2NNxZqiM&vet=12ahUKEwjT-qSH4sWGAxXsv4kEHXO0CBIQM3oECEkQAA..i&w=300&h=200&hcb=2&ved=2ahUKEwjT-qSH4sWGAxXsv4kEHXO0CBIQM3oECEkQAA
+    lousina, // Reference: https://storage.googleapis.com/ueat-assets/cb39ad7b-dd58-4adc-a6f4-4068ca788668.jpg
+    R, // Reference: https://www.google.com/imgres?q=ROAST%20BEEF%20%26%20CHEDDAR%20SUB%20mr%20sub&imgurl=https%3A%2F%2Fmrsub.ca%2Fwp-content%2Fuploads%2F2019%2F02%2FRoastBeefSub-300x200.jpg&imgrefurl=https%3A%2F%2Fmrsub.ca%2Fsub_categories%2Froast-beef-sub%2F&docid=NLWLfDtxoZ0kJM&tbnid=Z3YBB-smZjXqYM&vet=12ahUKEwio0eLJ4sWGAxW_lIkEHSC9ErAQM3oECBYQAA..i&w=300&h=200&hcb=2&ved=2ahUKEwio0eLJ4sWGAxW_lIkEHSC9ErAQM3oECBYQAA
+    Seafood, // Reference: https://storage.googleapis.com/ueat-assets/a44ec591-550a-4b2c-850b-f705ffdae82d.jpg
+    steak, // Reference: https://storage.googleapis.com/ueat-assets/52d248d9-e6ab-4d80-b83b-4ce4ce019c69.jpg
+    texmex, // Reference: https://www.google.com/url?sa=i&url=https%3A%2F%2Fmrsub.ca%2Fsub_categories%2Ftexmexblackbean%2F&psig=AOvVaw0GpzRo7ik8hVXWCzcrK3Ou&ust=1717721952071000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPDttpvjxYYDFQAAAAAdAAAAABAE
+    UltimateClub // Reference: https://www.google.com/imgres?q=ultimate%20clubmr%20sub&imgurl=https%3A%2F%2Fb2038387.smushcdn.com%2F2038387%2Fwp-content%2Fuploads%2F2019%2F01%2FUltimateClub12inchSub-300x200-1.jpg%3Flossy%3D1%26strip%3D1%26webp%3D1&imgrefurl=https%3A%2F%2Fmrsub.ca%2Fsub_categories%2Fultimateclub%2F&docid=KTSCqxBF5Qpf0M&tbnid=5v5e9OkGQ2ATbM&vet=12ahUKEwiup6Kn48WGAxXBHNAFHdmkJV4QM3oECBcQAA..i&w=300&h=200&hcb=2&ved=2ahUKEwiup6Kn48WGAxXBHNAFHdmkJV4QM3oECBcQAA
 }
