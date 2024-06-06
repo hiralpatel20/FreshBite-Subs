c7b0665dcc838e7f7fbf7bd9e51221a234ecab91
hiralpatel20 <hiralvpatel1061@gmail.com>
2024-06-06 15:46:39 -0400

    added code for cart.jsx

---
+++ frontend/src/pages/Cart/Cart.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import './Cart.css';
+import { useNavigate } from 'react-router-dom';
+import Footer from '../../components/Footer/Footer';
+import Navbar from '../../components/Navbar/Navbar';
+
+const Cart = () => {
+  // Here I added the sample data as of now
+  const [cartItems, setCartItems] = useState([
+    { id: 1, name: 'Item 1', price: 10, quantity: 1 },
+    { id: 2, name: 'Item 2', price: 20, quantity: 2 },
+    { id: 3, name: 'Item 3', price: 30, quantity: 1 }
+  ]);
+
+  // Here I added the function to handle the quantity change for a cart item
+  const handleQuantityChange = (id, quantity) => {
+    const updatedCartItems = cartItems.map(item => {
+      if (item.id === id) {
+        return { ...item, quantity: quantity < 1 ? 1 : quantity }; // Here I ensure that the quantity is not less than 1
+      }
+      return item;
+    });
+    setCartItems(updatedCartItems);
+  };
+
+  // Here I added the function for removing the item from the cart
+  const handleRemoveItem = (id) => {
+    const updatedCartItems = cartItems.filter(item => item.id !== id);
+    setCartItems(updatedCartItems);
+  };
+
+  // Here I added the function to count the total amount of items in the cart
+  const calculateTotal = () => {
+    return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+  };
+
+  // Here I use navigate hook to navigate to the checkout page
+  const navigate = useNavigate();
+  const handleCheckout = () => {
+    navigate('/Checkout');
+  };
+
+  return (
+  <>
+  <Navbar />
+    <div className="cart-page">
+      <h2>Shopping Cart</h2>
+      <div className="cart-items">
+        {cartItems.map(item => (
+          <div key={item.id} className="cart-item">
+            <div className="item-details">
+              <span>{item.name}</span>
+              <span>${item.price}</span>
+            </div>
+            <div className="item-actions">
+              <input
+                type="number"
+                value={item.quantity}
+                onChange={(e) => handleQuantityChange(item.id, parseInt(e.target.value))}
+              />
+              <button onClick={() => handleRemoveItem(item.id)}>Remove</button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="cart-total">
+        <h3>Total: ${calculateTotal()}</h3>
+        <button className="checkout-button" onClick={handleCheckout}>Proceed to Checkout</button>
+      </div>
+      <Footer />
+    </div>
+    </>
+  );
+};
+
+export default Cart;
