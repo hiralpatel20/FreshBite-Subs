9c38dadd27a8b289e4daf51f0898135b42159722
chelshiyapatel <chelshiyapatel26@gmail.com>
2024-07-01 17:20:44 -0400

    Added the necessary pacakages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+// Here I create a new ApolloClient instance
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',  // This is GraphQL server endpoint
+  cache: new InMemoryCache(), // Here I set Apollo cache
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  // Here I provide ApolloClient to the entire application
+  <ApolloProvider client={client}>  
+    <BrowserRouter>
       <App />
     </BrowserRouter>
+  </ApolloProvider>
 )
