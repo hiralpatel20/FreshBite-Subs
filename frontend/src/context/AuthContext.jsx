4c925a6a1b79df54ecc9ae7890291254e71a2eba
vidhimerai <vidhimerai16@gmail.com>
2024-08-14 19:51:36 -0400

    updated AuthProvider to get the user from local Storage

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,12 +1,21 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect  } from 'react'; 
 
 // Here I create the context for authentication
 const AuthContext = createContext();
 
 // This AuthProvider component is wrap the part of the app that needs to the authentication state
 const AuthProvider = ({ children }) => {
-  // Here I create the state t hold the current user, as of now I set it to the null
-  const [user, setUser] = useState(null);
+  // Here I created the state that holds the user, initaly is is null
+  const [user, setUser] = useState(() => {
+    // Load user from localStorage if available
+    const savedUser = localStorage.getItem('user');
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
+
+  useEffect(() => {
+    // This will save the user to localStorage
+    localStorage.setItem('user', JSON.stringify(user));
+  }, [user]);
 
   return (
     // Here I provide the user and setUser to the rest of the app
